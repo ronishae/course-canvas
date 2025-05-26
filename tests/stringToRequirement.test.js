4e95a6f46341d9099c667d92e24e6d776c26fa69
Roni <144291306+shaeroni@users.noreply.github.com>
2025-05-25 20:17:43 -0400

    deep nesting test case

--- tests/stringToRequirement.test.js
+++ tests/stringToRequirement.test.js
@@ -1,4 +1,6 @@
 const { stringToRequirement } = require('../AST'); 
+
+// npm test tests/stringToRequirement.test.js
 test('handles empty string', () => {
     const result = stringToRequirement('').toString();
     expect(result).toEqual('');
@@ -22,6 +24,11 @@ test('handles OR requirements', () => {
 test('handles nested requirements', () => {
     const result = stringToRequirement('CSC110 / (CSC111, CSC112)').toString();
     expect(result).toEqual('OR(CSC110, AND(CSC111, CSC112))');
+});
+
+test('handles deep nesting', () => {
+    const result = stringToRequirement('A / (B, (C, (D / (E1, E2)), (F, G)), H)').toString();
+    expect(result).toEqual('OR(A, AND(B, AND(C, OR(D, AND(E1, E2)), AND(F, G)), H))');
 });
 
 test('respects non list parentheses', () => {
